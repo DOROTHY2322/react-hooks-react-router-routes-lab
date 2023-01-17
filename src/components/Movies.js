6f13a4783343b6720a29805ba74dc5dfb770114d
DOROTHY2322 <kimelidorothy@gmail.com>
2023-01-17 20:21:23 +0300

    routers

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,36 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <h1>Movies Page</h1>;
+  const movies = [
+    {
+      title: "Doctor Strange",
+      time: 115,
+      genres: ["Action", "Adventure", "Fantasy"],
+    },
+    {
+      title: "Trolls",
+      time: 92,
+      genres: ["Animation", "Adventure", "Comedy", "Family", "Fantasy"],
+    },
+    {
+      title: "Jack Reacher: Never Go Back",
+      time: 118,
+      genres: ["Action", "Adventure", "Crime", "Mystery", "Thriller"],
+    },
+  ];
+
+  return (
+    <div>
+      <h1>Movies Page</h1>
+      {movies.map(movie => (
+        <div key={movie.title}>
+          <p>Title: {movie.title}</p>
+          <p>Time: {movie.time}</p>
+        </div>
+      ))}
+    </div>
+  );
 }
+
 
 export default Movies;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,13 +3,16 @@ import { NavLink } from "react-router-dom";
 
 function NavBar() {
   return (
-    <nav>
-      <NavLink to="/">Home</NavLink>
-      <NavLink to="/movies">Movies</NavLink>
-      <NavLink to="/directors">Directors</NavLink>
-      <NavLink to="/actors">Actors</NavLink>
-    </nav>
+    <div className="navbar">
+      <nav>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/movies">Movies</NavLink>
+        <NavLink to="/directors">Directors</NavLink>
+        <NavLink to="/actors">Actors</NavLink>
+      </nav>
+    </div>
   );
 }
+
 
 export default NavBar;
